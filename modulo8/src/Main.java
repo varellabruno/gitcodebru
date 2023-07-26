b2a18f2ff6c2f7551db8959e0aa199f14a28cf6b
Bruno Fernandes Varella <134634963+varellabruno@users.noreply.github.com>
2023-07-26 17:49:37 -0300

    Add files via upload

---
+++ modulo8/src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        float nota1 = 7.5F;
+        int nota2 = 8;
+        int nota3 = 5;
+        int nota4 = 9;
+
+        nota1 = (nota1 / 4 );
+        nota2 = (nota2 / 4 );
+        nota3 = (nota3 / 4 );
+        nota4 = (nota4 / 4 );
+
+        System.out.println("Média nota 1: " + " " + nota1);
+        System.out.println("Média nota 2: " + " " + nota2);
+        System.out.println("Média nota 3: " + " " + nota3);
+        System.out.println("Média nota 4: " + " " + nota4);
+
+    }
+}
