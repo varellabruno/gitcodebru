80b6db382aa1ea1019748c6b2b3db957f47451d7
Bruno Fernandes Varella <134634963+varellabruno@users.noreply.github.com>
2023-07-31 17:26:54 -0300

    Add files via upload

---
+++ Modulo09.java
@@ -0,0 +1,20 @@
+public class Modulo09 {
+
+    public static void main(String[] args) {
+
+        int valor = 10;
+        System.out.println("O valor é " + valor);
+
+
+        //Autoboxing
+        Integer valor1 = valor;
+        System.out.println("O valor é " + valor1);
+
+        //Casting
+        Integer valor2 = (int) valor;
+        System.out.println("O valor é " + valor2);
+
+
+    }
+
+}
