489a950bf23c4e2b00790e857d523b95270a7dbf
Bruno Fernandes Varella <134634963+varellabruno@users.noreply.github.com>
2023-07-31 15:41:31 -0300

    Update Main.java
    Corrigido

--- modulo8/src/Main.java
+++ modulo8/src/Main.java
@@ -2,20 +2,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        float nota1 = 7.5F;
+         double nota1 = 7.5;
         int nota2 = 8;
-        int nota3 = 5;
+        double nota3 = 5;
         int nota4 = 9;
+        double resultado;
 
-        nota1 = (nota1 / 4 );
-        nota2 = (nota2 / 4 );
-        nota3 = (nota3 / 4 );
-        nota4 = (nota4 / 4 );
+        resultado = (nota1 + nota2 + nota3 + nota4) / 4;
 
-        System.out.println("Média nota 1: " + " " + nota1);
-        System.out.println("Média nota 2: " + " " + nota2);
-        System.out.println("Média nota 3: " + " " + nota3);
-        System.out.println("Média nota 4: " + " " + nota4);
+
+        System.out.printf("A Média nota é: %.2f".formatted( resultado) );
 
     }
 }
